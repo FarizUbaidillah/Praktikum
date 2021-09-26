6a7a6984fc5212a6326ead78c7350ca1128ecf60
FarizUbaidillah <91111321+FarizUbaidillah@users.noreply.github.com>
2021-09-26 23:45:15 +0700

    Add files via upload

---
+++ Luas Bangunan1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 25 17:33:55 2021
+@NIM : 065.00.21.00.016
+@author: FARIZ UBAIDILLAH
+"""
+
+
+
+print("menghitung luas ruangan")
+
+p = float (input("panjang ruangan =  "))
+l = float (input("lebar ruangan =  "))
+y = input("meter/inci")
+print("meter")
+
+Luas = p*l
+print("luasnya adalah  = ", Luas, "meter")
+
