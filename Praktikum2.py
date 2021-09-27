e06fab80035cdb662317a9e6266b5a87aa23c0bf
FarizUbaidillah <91111321+FarizUbaidillah@users.noreply.github.com>
2021-09-27 15:10:17 +0700

    Add files via upload

---
+++ Praktikum2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 13:20:19 2021
+@NIM :065.00.21.00.016
+@author: FARIZ UBAIDILLAH
+"""
+
+
+#import math
+import math
+
+
+A = int(input("masukan nilai A = "))
+ 
+B = int(input("masukan nilai B  ="))
+
+      
+hasil_penambahan = A+B
+print ("hasil penambahan A dan B = ",hasil_penambahan)
+hasil_pengurangan = A-B
+print ("hasil pengurangan A dan B =",hasil_pengurangan)
+hasil_perkalian = A*B
+print("hasil perkalian A dan B adalah =",hasil_perkalian)
+
+print("hasil dari log A adalah=",math.log10(A))
+hasil_pembagian = A/B
+print("hasil pembagian A dan B =",hasil_pembagian)
+hasil_sisa_bagi = A%B
+print("hasil sisa bagi A dan B =",hasil_sisa_bagi)
+hasil_pemangkatan = A**B
+print("hasil pemangkatan A dan B =",hasil_pemangkatan)
